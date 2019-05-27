6a0cd72ac2885d640ef17da83e40cd19f7383efd
bai_da_ye <3232317182@qq.com>
2019-05-27 13:19:35 +0800

    第一次上传

---
+++ demo30/demo30.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2019-04-30T11:51:03
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = demo30
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        widget.cpp
+
+HEADERS  += widget.h
+
+FORMS    += widget.ui
---
+++ demo30/widget.cpp
@@ -0,0 +1,38 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    button = new QPushButton("Hallo" ,this);
+    button->setGeometry(100,100,100,100);
+    button->setStyleSheet(" QPushButton{border: 1px solid #C0C0C0; \
+                           background-color: white;\
+                           border-style: solid;\
+                           border-radius:0px;\
+                           color:black;\
+                           font-family:\"STXihei\";\
+                           font-size: 15px;\
+                           padding:0 0px;\
+                           margin:0 0px;\
+                           }\
+                           QPushButton:hover{border: 1px solid #C0C0C0; \
+                            background-color: #ececec;\
+                            border-style: solid;\
+                            border-radius:0px;\
+                            color:black;\
+                            font-family:\"STXihei\";\
+                            font-size: 15px;\
+                            padding:0 0px;\
+                            margin:0 0px;\
+                            }\
+                           ");
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
